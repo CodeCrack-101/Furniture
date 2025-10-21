de7b799a386c2e5c5914460b071cc4425245b48e
CodeCrack-101 <khanobaidurrahman91@gmail.com>
2025-10-21 14:00:25 +0530

    done

--- src/Component/Contact/Contact.jsx
+++ src/Component/Contact/Contact.jsx
@@ -27,7 +27,7 @@ const ContactUs = () => {
           <div className="contact-form">
             <h3>Contact Form</h3>
             <form action="https://api.web3forms.com/submit" method="post" >
-            <input type="hidden" name="access_key" value="e0998387-f0ae-498f-a3e5-379ff3fa7103"/> 
+            <input type="hidden" name="access_key" value="c4b5b87b-d096-46a9-b737-aa993db83b18"/> 
               <input type="text" name="name" placeholder="Your Name" required />
               <input type="email" name="email" placeholder="Your Email" />
               <input
