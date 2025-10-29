747af5893e98d0d9b0162af3820d0eec9972865c
shaikhzaid0820-prog <shaikhzaid0820@gmail.com>
2025-10-29 11:29:58 +0530

    Done

--- src/Component/Body2/B.jsx
+++ src/Component/Body2/B.jsx
@@ -5,15 +5,15 @@ import { useNavigate } from 'react-router-dom';
 const allProducts = [
   { id: 1, name: 'Boss Chair', category: 'Chair', imageUrl: 'https://5.imimg.com/data5/SELLER/Default/2023/10/350488058/SQ/KM/MG/7199438/boss-hb-500x500.png' },
   { id: 2, name: 'Boss Mesh Chair', category: 'Chair', imageUrl: 'https://www.bestbuy-officechairs.co.uk/media/img/shop/srcpd/013001.jpg' },
-  { id: 3, name: 'Boss Mesh Work Chair', category: 'Chair', imageUrl: 'https://img.ofo.co.uk/media/img/shop/srcpd/122790.jpg' },
+  { id: 3, name: 'Boss Mesh Work Chair', category: 'Chair', imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQZ0x-2m4ffsj1-gHwzRzf6rTLpSwgsACMzw2Dm-G8wXnWvocSEupcQTfTQfcpukXGOirU&usqp=CAU' },
   { id: 4, name: 'Lounge Chair', category: 'Chair', imageUrl: 'https://www.josmostudio.com/cdn/shop/files/1_84.webp?v=1752143351&width=1946' },
   { id: 5, name: 'Visitor Chair', category: 'Chair', imageUrl: 'https://5.imimg.com/data5/MY/FT/MY-41397653/visitor-chairs-ocz-182-250x250.jpg' },
-  { id: 6, name: 'Dining Chair', category: 'Chair', imageUrl: 'https://www.lunafurn.com/cdn/shop/products/2243S_noBG_side1.jpg?v=1669864785&width=1214' },
+  { id: 6, name: 'Dining Chair', category: 'Chair', imageUrl: 'https://media.furniturerow.com/image/list/bl_optimize/t_sku_image2/w_1024/f_jpg/CS-HCHTCY.json' },
   { id: 7, name: 'Cafe Chair', category: 'Chair', imageUrl: 'https://5.imimg.com/data5/MY/FT/MY-41397653/visitor-chairs-ocz-182-250x250.jpg' },
-  { id: 8, name: 'Bar Chair', category: 'Chair', imageUrl: 'https://5.imimg.com/data5/YR/SI/MY-39072337/the-ergonomico-bar-stool-black-500x500-500x500.jpg' },
+  { id: 8, name: 'Bar Chair', category: 'Chair', imageUrl: 'https://sinhupfatt.com/wp-content/uploads/2024/01/BS-717-1.jpg' },
   { id: 9, name: 'Waiting Chair', category: 'Chair', imageUrl: 'https://cdn.moglix.com/p/xAbeZSiMiuqYm-xxlarge.jpg' },
   { id: 10, name: 'Sofa', category: 'Sofa', imageUrl: 'https://cdn.shopify.com/s/files/1/0758/7397/6610/files/75001-38-SD-P1-KO_1_large.jpg?v=1715376191' },
-  { id: 11, name:'Steel Cupboard', category: 'Cupboard', imageUrl: 'https://m.media-amazon.com/images/I/51wjnEJgxOL.jpg' },
+  { id: 11, name:'Steel Cupboard', category: 'Cupboard', imageUrl: 'https://m.media-amazon.com/images/I/51LjfLHB55L.jpg' },
 ];
 
 const ProductSearch = () => {
