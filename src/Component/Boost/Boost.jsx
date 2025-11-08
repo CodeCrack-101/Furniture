4036abcdbf808c2aee3610424e99104b52134450
CodeCrack-101 <khanobaidurrahman91@gmail.com>
2025-11-08 10:38:11 +0530

    boost

--- src/Component/Boost/Boost.jsx
+++ src/Component/Boost/Boost.jsx
@@ -5,7 +5,7 @@ function OverlapLayout() {
   return (
     <div className="overlap-container">
         <p>
-          Shiri G Furniture offers a wide range of premium office chairs, stylish
+          Shiri G Best office Chair In Mumbai, offers a wide range of premium office chairs, stylish
           sofas, and comfortable cots and beds — designed to blend comfort,
           durability, and modern style.
         </p>
